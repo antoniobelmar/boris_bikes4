3d4bcfe30c463fa8073f4d69eed1ed8e5e4009cf
antonio <antoniobelmardacosta@gmail.com>
2017-10-26 14:38:08 +0100

    adds a test for docking station release.bike method to release an instance of class bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,11 @@
 require "docking_station"
 
+
 describe DockingStation do
   it 'responds to release_bike' do
-   expect(DockingStation.new).to respond_to(:release_bike)
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
+  it "ensures release_bike gets user new bike" do
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
   end
 end
