af21a6507dbdfa5b11a1e7e285d0429302caf250
antonio <antoniobelmardacosta@gmail.com>
2017-10-26 22:57:49 +0100

    defines full? and empty? methods and refactors code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,16 +8,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no bikes at this docking station. Sorry!" unless @bikes.count>0
+    raise "There are no bikes at this docking station. Sorry!" if empty?
     @bikes
   end
 
   def dock(bike)
-    if @bikes.count<20
-      @bikes.push(bike)
-    else
-      raise "This docking station is full, please try a different one!"
-    end
+    raise "This docking station is full, please try a different one!" if full?
+    @bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @bikes.count>=20
+  end
+
+  def empty?
+    @bikes.count==0
   end
 
 end
